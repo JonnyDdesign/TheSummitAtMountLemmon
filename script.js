2736550c894e12c580b9a6882937486034b95ee5
[Jon Desserres] <[jondesserres@gmail.com]>
2023-12-06 10:35:12 -0700

    created accessible hamburger menu for mobile view and adjusted some styling changes for larger screens

--- script.js
+++ script.js
@@ -1,6 +1,27 @@
-let toggleButton = document.getElementsByClassName('toggle-button')[0];
+const menuToggle = document.querySelector('.menu-toggle');
+const siteNavigation = document.querySelector('.primary-navigation');
+
+menuToggle.addEventListener('click', () => {
+  const isOpened = menuToggle.getAttribute('aria-expanded') === "true";
+  isOpened ? closeMenu() : openMenu();
+});
+
+function openMenu() {
+  menuToggle.setAttribute('aria-expanded', "true");
+  siteNavigation.setAttribute('data-state', "opened");
+}
+function closeMenu() {
+  menuToggle.setAttribute('aria-expanded', "false");
+  siteNavigation.setAttribute('data-state', "closing");
+
+  siteNavigation.addEventListener('animationend', () => {
+    siteNavigation.setAttribute('data-state', "closed");
+  }, {once: true})
+}
+
+/*let toggleButton = document.getElementsByClassName('toggle-button')[0];
 let navbarLinks = document.getElementsByClassName('navbar-links')[0];
 
 toggleButton.addEventListener('click', () => {
     navbarLinks.classList.toggle('active')
-})                    
+})*/        
